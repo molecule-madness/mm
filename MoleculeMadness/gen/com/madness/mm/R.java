a4702194e54659c7e8c1bc73d229aa05ddeb26d3
steve <skk8768@rit.edu>
2013-10-18 16:49:44 -0400

    made demo hint screen

--- MoleculeMadness/gen/com/madness/mm/R.java
+++ MoleculeMadness/gen/com/madness/mm/R.java
@@ -19,23 +19,40 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int hint_btn_min_width=0x7f040003;
+        public static final int hint_question_text_size=0x7f040002;
+        public static final int hint_spacer=0x7f040004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f080004;
+        public static final int button3=0x7f080005;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main_menu=0x7f030000;
+        public static final int activity_hint=0x7f030000;
+        public static final int activity_main_menu=0x7f030001;
     }
     public static final class menu {
-        public static final int main_menu=0x7f070000;
+        public static final int hint=0x7f070000;
+        public static final int main_menu=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int hint_def_btn_A=0x7f050006;
+        public static final int hint_def_btn_B=0x7f050007;
+        public static final int hint_def_btn_C=0x7f050008;
+        public static final int hint_default_question=0x7f050005;
+        public static final int hint_instruction=0x7f050004;
+        public static final int title_activity_hint=0x7f050003;
     }
     public static final class style {
         /** 
