ef575f052d7a9eb032628c0d0db0c44eb5f634dd
steve <skk8768@rit.edu>
2013-10-19 12:14:57 -0400

    added HintActivity and associated xml files to project

---
+++ MoleculeMadness/src/com/madness/mm/HintActivity.java
@@ -0,0 +1,22 @@
+package com.madness.mm;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HintActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hint);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hint, menu);
+		return true;
+	}
+
+}
