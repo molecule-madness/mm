623d36018b2ad5142b402d41af1505e2e8c73cea
steve <skk8768@rit.edu>
2013-10-19 12:55:41 -0400

    hint screen is now dynamically generated

--- MoleculeMadness/src/com/madness/mm/HintActivity.java
+++ MoleculeMadness/src/com/madness/mm/HintActivity.java
@@ -1,15 +1,23 @@
 package com.madness.mm;
 
+import com.madness.mm.model.Hint;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class HintActivity extends Activity {
+
+	private LinearLayout ansLayout;
+	private Button[] btns;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hint);
+		
+		initView();
 	}
 
 	@Override
@@ -17,6 +25,23 @@ public class HintActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.hint, menu);
 		return true;
+	}
+
+	private void initView() {
+		setContentView(R.layout.activity_hint);
+
+		btns = new Button[Hint.Q_PER_HINT];
+		
+		ansLayout = (LinearLayout) findViewById(R.id.ansLayout);
+		
+		for (int i = 0; i < btns.length; i++) {
+			Button b = new Button(this);
+			b.setMinWidth(R.dimen.hint_btn_min_width);
+			b.setText(R.string.hint_btn_def_txt);
+			
+			btns[i] = b;
+			ansLayout.addView(b);
+		}
 	}
 
 }
---
+++ MoleculeMadness/src/com/madness/mm/model/Hint.java
@@ -0,0 +1,5 @@
+package com.madness.mm.model;
+
+public class Hint {
+	public static final int Q_PER_HINT = 3;
+}
