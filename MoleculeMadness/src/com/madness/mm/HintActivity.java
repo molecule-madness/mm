4ef77ee5552d62e10b1d2c9675d124a578859f5a
steve <skk8768@rit.edu>
2013-10-19 13:55:23 -0400

    Added hint activity that returns with hint result

--- MoleculeMadness/src/com/madness/mm/HintActivity.java
+++ MoleculeMadness/src/com/madness/mm/HintActivity.java
@@ -1,22 +1,38 @@
 package com.madness.mm;
 
 import com.madness.mm.model.Hint;
+import com.madness.mm.model.HintFormattable;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HintActivity extends Activity {
+public class HintActivity extends Activity implements HintFormattable {
+
+	public static final Integer HINT_PASS = 0;
+	public static final Integer HINT_FAIL = 1;
 
 	private LinearLayout ansLayout;
-	private Button[] btns;
+	private OrderedButton[] btns;
+
+	private TextView hintQuestion;
+
+	private Hint curHint;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		setContentView(R.layout.activity_hint);
+		hintQuestion = (TextView) findViewById(R.id.hintQuestion);
+		ansLayout = (LinearLayout) findViewById(R.id.ansLayout);
+
 		initView();
 	}
 
@@ -28,20 +44,75 @@ public class HintActivity extends Activity {
 	}
 
 	private void initView() {
-		setContentView(R.layout.activity_hint);
+		btns = new OrderedButton[Hint.Q_PER_HINT];
 
-		btns = new Button[Hint.Q_PER_HINT];
-		
-		ansLayout = (LinearLayout) findViewById(R.id.ansLayout);
-		
 		for (int i = 0; i < btns.length; i++) {
-			Button b = new Button(this);
+			final OrderedButton b = new OrderedButton(this, i);
 			b.setMinWidth(R.dimen.hint_btn_min_width);
 			b.setText(R.string.hint_btn_def_txt);
+			
+			b.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					
+					Context context = getApplicationContext();
+					int duration = Toast.LENGTH_SHORT;
+					int resId;
+					
+					if(b.equals(curHint)) {
+						setResult(HINT_PASS);
+						resId = R.string.hint_pass_toast;
+					}
+					else {
+						setResult(HINT_FAIL);
+						resId = R.string.hint_fail_toast;
+					}
+					
+					Toast.makeText(context, resId, duration).show();
+					finish();
+				}
+			});
 			
 			btns[i] = b;
 			ansLayout.addView(b);
 		}
 	}
 
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		curHint = Hint.getRandHint();
+		curHint.format(this);
+	}
+
+	@Override
+	public void setAnswerText(int ansIndex, String answer) {
+		btns[ansIndex].setText(answer);
+	}
+
+	@Override
+	public void setQuestionText(String question) {
+		hintQuestion.setText(question);
+	}
+
+	private class OrderedButton extends Button {
+		private int index;
+
+		public OrderedButton(Context context, int index) {
+			super(context);
+			this.index = index;
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			if (other instanceof Hint) {
+				Hint that = (Hint) other;
+
+				return that.getCorrect() == this.index;
+			}
+			return false;
+		}
+	}
 }
--- MoleculeMadness/src/com/madness/mm/model/Hint.java
+++ MoleculeMadness/src/com/madness/mm/model/Hint.java
@@ -1,5 +1,56 @@
 package com.madness.mm.model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Hint {
 	public static final int Q_PER_HINT = 3;
+
+	private String question;
+	private int correct;
+	private String[] answers;
+
+	public Hint(String question, int correct, String... answers) {
+		this.question = question;
+		this.correct = correct;
+		this.answers = answers;
+
+		//assert (answers.length == Q_PER_HINT);
+	}
+
+	public void format(HintFormattable hf) {
+		hf.setQuestionText(question);
+
+		for (int i = 0; i < answers.length; i++) {
+			hf.setAnswerText(i, answers[i]);
+		}
+	}
+
+	public int getCorrect() {
+		return correct;
+	}
+
+	public static ArrayList<Hint> hints;
+
+	private static Random r;
+	
+	private static void initHints() {
+		hints = new ArrayList<Hint>();
+		
+		hints.add(new Hint("What Johhny thought was H20 was actually:", 2,
+				"Water", "Nitrogen", "H2SO4"));
+		
+		hints.add(new Hint("What molecule sustains life?", 0,
+				"Water", "Nitrogen", "H2SO4"));
+		
+		r = new Random();
+	}
+	
+	public static Hint getRandHint() {
+		if(hints == null) {
+			initHints();
+		}
+		
+		return hints.get(r.nextInt(hints.size()));
+	}
 }
