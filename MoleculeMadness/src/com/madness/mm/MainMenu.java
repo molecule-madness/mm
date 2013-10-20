b1082b094943f00fc5fb2d7a614e55aa5e374d07
steve <skk8768@rit.edu>
2013-10-20 14:27:53 -0400

    Adding test activity from which to launch current screen under test

--- MoleculeMadness/src/com/madness/mm/MainMenu.java
+++ MoleculeMadness/src/com/madness/mm/MainMenu.java
@@ -15,39 +15,37 @@ import com.madness.mm.model.Hint;
 
 public class MainMenu extends Activity {
 
-
-
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
-        Button start_Button = (Button)findViewById(R.id.Start_button);
+		Button start_Button = (Button) findViewById(R.id.Start_button);
 
-        // will make a quiz when its clicked
-        start_Button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+		// will make a quiz when its clicked
+		start_Button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
 
-            }
-        });
+			}
+		});
 
-        Button stats_Button = (Button)findViewById(R.id.Stats_Button);
-        // will go to the stats page when clicked
-        stats_Button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
+		Button stats_Button = (Button) findViewById(R.id.Stats_Button);
+		// will go to the stats page when clicked
+		stats_Button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Perform action on click
+			}
+		});
 
+		Button instructions = (Button) findViewById(R.id.Instruction_Button);
+		// will go the instructions page when clicked
+		instructions.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goto_Instructions = new Intent();
+				startActivity(goto_Instructions);
+			}
+		});
+	}
 
-        Button instructions = (Button)findViewById(R.id.Instruction_Button);
-        // will go the instructions page when clicked
-        instructions.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent goto_Instructions = new Intent()
-                        startActivity(goto_Instructions);
-            }
-        });
-    }
-public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
