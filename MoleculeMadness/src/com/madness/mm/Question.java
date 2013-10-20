42c2be75a3316e1545e74b5a53094b2e6a0648d5
Gangus <GangusKahn@gmail.com>
2013-10-20 15:20:18 -0400

    firx quiz a little and deleted newgame

---
+++ MoleculeMadness/src/com/madness/mm/Question.java
@@ -0,0 +1,14 @@
+package com.madness.mm;
+
+/* @author Alex Kahn
+ * @className Question.java
+ * @date 10/20/2013
+ * @classDescription- This class will be a pojo for the questions that the
+ * builder will use
+ */
+public class Question {
+    String answer;
+    public Question(){
+
+    }
+}
